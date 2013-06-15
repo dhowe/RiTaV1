c86644b39e9090c0b936656ffefdc30251226ecd
Daniel C. Howe <daniel.c.howe@gmail.com>
2013-06-16 02:01:21 +1000

    new dynamic casts
    Former-commit-id: 6ea93b2301c56910bc0cbe70f2525634d97a5cdb

---
+++ src/rita/support/RiDynamic.java
@@ -0,0 +1,99 @@
+package rita.support;
+
+import java.lang.reflect.Method;
+
+import rita.RiTa;
+import rita.RiTaException;
+
+/**
+ * Implements dynamic typing (eg 'duck-typing') for RiTa objects -- allowing programs to treat
+ * objects from separate hierarchies and packages as the same, assuming they
+ * implement the specified interfaces (checked only at runtime)
+ */
+public class RiDynamic
+{  
+  protected Object delegate;
+
+  /* public RiDynamic(Class dynamicProxyClass, Class iface) {
+    this((Object)dynamicProxyClass, iface);
+  }
+  public RiDynamic(Object dynamicProxy, Class iface) {
+    this(dynamicProxy, new Class[]{ iface });
+  } */
+  
+  public RiDynamic(Class dynamicProxyClass, Class ... ifaces) {
+    this((Object)dynamicProxyClass, ifaces);
+  }
+
+  public RiDynamic(Object dynamicProxy, Class ... ifaces) {
+    dynamicProxy = multiGetClass(dynamicProxy);
+    //System.out.println("RiDynamicObject.RiDynamicObject2("+dynamicProxy+","+RiTa.asList(ifaces)+")");
+    for (int i = 0; i < ifaces.length; i++) {
+      if (!instanceOf(ifaces[i], dynamicProxy)) 
+        throw new RiTaException(dynamicProxy.getClass()
+            +" cannot dynamically implement "+ifaces[i].getCanonicalName());
+    }    
+    delegate = RiInvocation.implement(ifaces, dynamicProxy);  
+  } 
+  
+  /**
+   * Indicates if an object is a Dynamic (DuckTyped) instance of an interface. 
+   * 
+   * @param iface The interface to implement
+   * @param object The object to test
+   * @return true if every method in the interface is present in the object, 
+   *         otherwise false
+   */
+  public static boolean instanceOf(Class iface, Object object) {
+    //System.out.println("RiDynamicType.instanceOf("+object+")"); 
+    final Method[] methods = iface.getMethods();
+    Class candClass = object.getClass();
+    for (int i = 0; i < methods.length; i++) {
+      Method method = methods[i];
+      //System.out.println(i+") "+methods[i]);
+      try {
+        candClass.getMethod(method.getName(), method.getParameterTypes());        
+      } catch (NoSuchMethodException e) {
+        //System.err.println("[ERROR] no match for method: "+method.getName()+"("+RiTa.asList(method.getParameterTypes())+");");
+        return false;     
+      } catch (Throwable e) {
+        throw new RiTaException("Unexpected Exception! no match for method: "+method.getName()+"("+RiTa.asList(method.getParameterTypes())+");");
+      }        
+    }
+    return true;
+  }
+  
+  private static Object multiGetClass(Object o) {
+    try {
+      if (o instanceof Class) 
+        return ((Class)o).newInstance();     
+      return o;      
+    } catch (Exception e) {
+      throw new RiTaException(e);
+    }   
+  } 
+
+  public Object getDelegate() {
+    return delegate;
+  }
+  
+  /**
+   * Returns the passed in Object after dynamically casting
+   * it to the specified interfaces.<br>
+   * Note: 'toCast' must be a an instance of a public class.
+   */
+  public static Object cast(Object toCast, Class ... ifaces) {
+    return new RiDynamic(toCast, ifaces).getDelegate();
+  }
+  
+  public static void main(String[] args) {
+    Object o = new Object() {
+      public void run() {
+        System.out.println("run() called");
+      }
+    };
+    RiDynamic rdo = new RiDynamic(o, Runnable.class);
+    Runnable r = (Runnable)rdo.delegate;
+    r.run();
+  }
+}// end
---
+++ src/rita/support/RiInvocation.java
@@ -0,0 +1,75 @@
+package rita.support;
+
+import java.lang.reflect.*;
+
+import rita.RiTaException;
+
+/**
+ * A proxy invocation handler that enables dynamic typing for RiTa objects
+ */
+public class RiInvocation implements InvocationHandler 
+{
+  protected Object object;
+  protected Class objectClass;
+  
+  protected RiInvocation(Object object) {
+    this.object = object;
+    this.objectClass = object.getClass();
+  }
+
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable 
+  {    
+    Method realMethod = null;
+    try {
+      realMethod = objectClass.getMethod(method.getName(), method.getParameterTypes());
+      return realMethod.invoke(object, args);
+    } 
+    catch (Exception e) {
+      String msg = null;
+      if (e instanceof java.lang.IllegalAccessException)
+        msg = "[ERROR] Make sure the class you are trying to "
+          +"(dynamically) cast is publicly defined.\n          "
+          +"----------------------------------------------------\n";
+      throw new RiTaException(msg, e);        
+    }     
+  }
+
+  /**
+   * Causes object to implement the interface and returns an instance
+   * of the object implementing interface even if
+   * interface was not declared in object.getClass()'s implements
+   * declaration.
+   * 
+   * This works as long as all methods declared in interface are
+   * present in the object.
+   * 
+   * @param iface
+   *          The Java class of the interface to implement
+   * @param object
+   *          The object to force to implement the interface
+   *          
+   * @return the object, but now implementing the interface
+   */
+  public static Object implement(Class iface, Object object) {
+    return implement(new Class[] { iface }, new RiInvocation(object));
+  }
+  
+  /**
+   * Causes object to implement the listed interfaces. Is succesful
+   * is all methods declared in the interfaces are present in the object.
+   * 
+   * @param ifaces
+   *          an array of Java classes representing the interfaces to implement
+   * @param object
+   *          The object to force to implement the interfaces
+   *          
+   * @return the object, but now implementing the interfaces
+   */
+  static Object implement(Class[] ifaces, Object object) {
+    Object o = Proxy.newProxyInstance
+      (object.getClass().getClassLoader(), ifaces, new RiInvocation(object));
+    //System.out.println("RiDynamicObject.implement() -> "+o);
+    return o;
+  }
+  
+}// end
