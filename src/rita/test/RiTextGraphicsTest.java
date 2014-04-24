329c412f0ee242ab305d6aaa705c72d5f6b4ada3
Kenny Wong <kennyviperhk@gmail.com>
2014-04-24 20:22:42 +0800

    graphics updated, RiTextGraphicsTest updated
    Former-commit-id: eae09d9079e64697740b7a72ed2837a3c37a24ab

--- src/rita/test/RiTextGraphicsTest.java
+++ src/rita/test/RiTextGraphicsTest.java
@@ -1,35 +1,104 @@
 package rita.test;
 
+
+
 import org.junit.Test;
 
 public class RiTextGraphicsTest
 {
-  static final String PATH = "/Users/dhowe/Documents/eclipse-workspace/RiTa/src/";
+	static String workingdirectory = System.getProperty("user.dir"); 
 
-  @Test
-  public void testSimplest()
-  {
-    String testName = "rita.render.test.Simplest";
-    PixelCompare pc = new PixelCompare(PATH);
-    pc.assertEqual(testName);
-  }
-  
-    @Test
-  public void testBoundingBoxes()
-  {
-    String testName = "rita.render.test.BoundingBoxes";
-    PixelCompare pc = new PixelCompare(PATH);
-    pc.assertEqual(testName);
-  }
-    
-  // continue with this pattern for all tests in package
-  
-  public static void main(String[] args)
-  {
-    String testName = "rita.render.test.Simplest";
-    PixelCompare pc = new PixelCompare(PATH);
-    //pc.generateRefFile(testName);
-    pc.visualDiff(testName);
-  }
+	//static final String PATH = "/Users/dhowe/Documents/eclipse-workspace/RiTa/src/";
+	static final String PATH = workingdirectory +"/src/";
+
+	@Test
+	public void testBoundingBoxes()
+	{
+		String testName = "rita.render.test.BoundingBoxes";
+		PixelCompare pc = new PixelCompare(PATH);
+		pc.assertEqual(testName);
+	}
+
+	@Test
+	public void testCreateLines()
+	{
+		String testName = "rita.render.test.CreateLines";
+		PixelCompare pc = new PixelCompare(PATH);
+		pc.assertEqual(testName);
+	}
+
+	@Test
+	public void testDefaultFont()
+	{
+		String testName = "rita.render.test.DefaultFont";
+		PixelCompare pc = new PixelCompare(PATH);
+		pc.assertEqual(testName);
+	}
+
+	@Test
+	public void testFontVlwTest()
+	{
+		String testName = "rita.render.test.FontVlwTest";
+		PixelCompare pc = new PixelCompare(PATH);
+		pc.assertEqual(testName);
+	}
+
+	@Test
+	public void testLayoutWithBreaks()
+	{
+		String testName = "rita.render.test.LayoutWithBreaks";
+		PixelCompare pc = new PixelCompare(PATH);
+		pc.assertEqual(testName);
+	}
+
+
+
+	@Test
+	public void testSimplest()
+	{
+		String testName = "rita.render.test.Simplest";
+		PixelCompare pc = new PixelCompare(PATH);
+		pc.assertEqual(testName);
+	}
+
+
+	@Test
+	public void testSplitText()
+	{
+		String testName = "rita.render.test.SplitText";
+		PixelCompare pc = new PixelCompare(PATH);
+		pc.assertEqual(testName);
+	}
+
+	@Test
+	public void testTextLayout()
+	{
+		String testName = "rita.render.test.TextLayout";
+		PixelCompare pc = new PixelCompare(PATH);
+		pc.assertEqual(testName);
+	}
+
+	@Test
+	public void testWordsLettersLines()
+	{
+		String testName = "rita.render.test.WordsLettersLines";
+		PixelCompare pc = new PixelCompare(PATH);
+		pc.assertEqual(testName);
+	}
+
+
+
+
+	// continue with this pattern for all tests in package
+
+	public static void main(String[] args)
+	{
+		String testName = "rita.render.test.BoundingBoxes";
+		PixelCompare pc = new PixelCompare(PATH);
+		pc.generateRefFile(testName);
+		//pc.visualDiff(testName);
+
+
+	}
 
 }
