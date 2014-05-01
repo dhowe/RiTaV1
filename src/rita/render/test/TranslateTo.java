22904a530dfc914d3accb8824f501a2ba630ed43
Kenny Wong <kennyviperhk@gmail.com>
2014-05-01 21:04:06 +0800

    RiText Tests updated
    Former-commit-id: a4fe8133982e1eee3e80da29b4d4277019c0aa74

--- src/rita/render/test/TranslateTo.java
+++ src/rita/render/test/TranslateTo.java
@@ -20,40 +20,43 @@ public class TranslateTo extends PApplet
     
     RiText.defaultFont("Georgia", 30);
 
-    RiText rt1 = new RiText(this, "ZIG");
+    RiText rt1 = new RiText(this, "Move",750,750);
 
-    RiText rt2 = new RiText(this, "ZAG");
 
-    rt2.motionType(RiTa.EASE_IN_OUT);
+    rt1.moveTo(200,50,2);
 
-    moveTo(rt1);
-    moveTo(rt2);
 
-    rt3 = new RiText(this, "ZOP");
-    rt3.position(0, 550);
+    rt3 = new RiText(this, "Move Delay2s");
+    rt3.position(200, 250);
    
-    rt3.moveTo(550, 0, 5,5);
+    rt3.moveTo(550, 50, 2,2);
 
-    rt4 = new RiText(this, "TURN",330,100);
-    rt4.rotate(50);
+    rt4 = new RiText(this, "Rotate",100,300);
+    rt4.rotate(50).fill(255,0,0);
 
-    rt5 = new RiText(this, "TURN Delay",150,500);
-    rt5.rotateTo(2,6,5);
+    rt5 = new RiText(this, "Rotate Delay2s",500,300);
+    rt5.fill(255,0,0);
+    rt5.rotateTo(-2,2,2);
     
-    rt6 = new RiText(this, "TURNNNN",width/2,height/2);
-    rotateTo(rt6);
+    rt6 = new RiText(this, "Rotate",300,300);
+    rt6.fill(255,0,0);
+    rt6.rotateTo(0.5f,1);
     
-    rt7 = new RiText(this, "Bigger",width-200,height-500);
+    rt7 = new RiText(this, "Bigger",50,500);
+    rt7.fill(0,200,0);
     rt7.scale(1.5f);
     
-    rt8 = new RiText(this, "Taller",width-200,height-400);
+    rt8 = new RiText(this, "Taller",300,500);
+    rt8.fill(0,200,0);
     rt8.scale(1.5f,2f);
     
-    rt9 = new RiText(this, "O",width-200,height-300);
-    rt9.scaleTo(5,2);
+    rt9 = new RiText(this, "Scale",50,700);
+    rt9.fill(0,200,0);
+    rt9.scaleTo(1.5f,2);
     
-    RiText rt10 = new RiText(this, "O delay",width-200,height-200);
-    rt10.scaleTo(5,2,5);
+    RiText rt10 = new RiText(this, "Scale Delay2s",300,700);
+    rt10.fill(0,200,0);
+    rt10.scaleTo(1.5f,2,2);
     
     
   }
@@ -62,45 +65,28 @@ public class TranslateTo extends PApplet
   {
     fill(255, 100); // leave trails
     rect(0, 0, width, height);
-    counter+=10;
-    fill(0);
-    text((int)rt3.position()[0] + ", "+  (int)rt3.position()[1] ,100,580);
     
-    text(rt4.rotate()[0] + "",430,100);
+    fill(0,0,255);
+    text((int)rt3.position()[0]+", "+(int)rt3.position()[1],(int)rt3.position()[0],(int)rt3.position()[1]+50);
+    
+    text(rt5.rotate()[0] + "",500,300+100);
     RiText.drawAll();
     
-    if(counter > width   ){
-		  counter=0;
-	  }
+
   }
 
   public void onRiTaEvent(RiTaEvent re) {
 
-		if (re.type() == RiTa.MOVE_TO)
+	/*	if (re.type() == RiTa.MOVE_TO)
 	     moveTo((RiText) re.source());
 		
 		if (re.type() == RiTa.ROTATE_TO)
 		     rotateTo((RiText) re.source());
-		
+		*/
   }
   
-  public void moveTo(RiText rt)
-  {
-	
-    float newX = counter;
-    float newY = counter;
-    float dst = rt.distanceTo(newX, newY);
 
-    rt.moveTo(newX, newY, dst / width);
-  }
-  
-  public void rotateTo(RiText rt)
-  {
 
-    float newRot = counter/PI;
-
-    rt.rotateTo(newRot,1);
-  }
   
 
 
