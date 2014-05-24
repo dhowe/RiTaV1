98b713c4b2dfe5903474337c4bcb0b4bcf7580f5
Daniel Howe <daniel@rednoise.org>
2014-05-24 22:55:44 +0800

    fix to graphics tests
    Former-commit-id: 160f2b79474df21ad3722dec4379831331fa6013

--- src/rita/test/RiTextGraphicsTest.java
+++ src/rita/test/RiTextGraphicsTest.java
@@ -9,76 +9,66 @@ import rita.render.test.misc.TextLayout;
 
 public class RiTextGraphicsTest // TODO: how to run these on JS tests?
 {
-	static String testPath = "/src/rita/render/test";
-	static String homeDir = System.getProperty("user.home");
-	static String project = homeDir + "/Documents/eclipse-workspace/RiTa";
+  static String testPath = "/src/rita/render/test";
+  static String homeDir = System.getProperty("user.home");
+  static String project = homeDir + "/Documents/eclipse-workspace/RiTa";
 
+  public static final String PATH = project + "/src/";
 
-	  public static final String PATH = project + "/src/";
-	  //Kenny
-	 // static String projectDir = System.getProperty("user.home");
-	 // public static final String PATH = projectDir +"/src/";
-	 
-	@After // run this after each test
-	public void cleanup()
-	{
-		RiText.resetDefaults();
-	}
-	
-	@Test
-	public void testBoundingBoxAndAlpha()
-	{
-		PixelCompare pc = new PixelCompare(PATH);
-		pc.assertEqual(BoundingBoxAndAlpha.class.getName());
-	}
-	
+  @After
+  // run this after each test
+  public void cleanup()
+  {
+    RiText.resetDefaults();
+  }
 
-	@Test
-	public void testBoundingBoxFeatures()
-	{
-		PixelCompare pc = new PixelCompare(PATH);
-		pc.assertEqual(BoundingBoxFeatures.class.getName());
-	}
+  @Test
+  public void testBoundingBoxAndAlpha()
+  {
+    PixelCompare pc = new PixelCompare(PATH);
+    pc.assertEqual(BoundingBoxAndAlpha.class.getName());
+  }
 
-	
-	@Test
-	public void testDefaultFontAndFontVlwTtf()
-	{
-		PixelCompare pc = new PixelCompare(PATH);
-		pc.assertEqual(DefaultFontAndFontVlwTtf.class.getName());
-	}
-	
+  @Test
+  public void testBoundingBoxFeatures()
+  {
+    PixelCompare pc = new PixelCompare(PATH);
+    pc.assertEqual(BoundingBoxFeatures.class.getName());
+  }
 
-	@Test
-	public void CreateLinesAndLayoutWithBreaks()
-	{
-		PixelCompare pc = new PixelCompare(PATH);
-		pc.assertEqual(CreateLinesAndLayoutWithBreaks.class.getName());
-	}
-	
-	@Test
-	public void testSimplest()
-	{
-		PixelCompare pc = new PixelCompare(PATH);
-		pc.assertEqual(Simplest.class.getName());
-	}
-	
+  @Test
+  public void testDefaultFontAndFontVlwTtf()
+  {
+    PixelCompare pc = new PixelCompare(PATH);
+    pc.assertEqual(DefaultFontAndFontVlwTtf.class.getName());
+  }
 
+  @Test
+  public void testCreateLinesAndLayoutWithBreaks()
+  {
+    PixelCompare pc = new PixelCompare(PATH);
+    pc.assertEqual(CreateLinesAndLayoutWithBreaks.class.getName());
+  }
 
-	@Test
-	public void testWordsLettersLines()
-	{
-		PixelCompare pc = new PixelCompare(PATH);
-		pc.assertEqual(WordsLettersLines.class.getName());
-	}  
+  @Test
+  public void testSimplest()
+  {
+    PixelCompare pc = new PixelCompare(PATH);
+    pc.assertEqual(Simplest.class.getName());
+  }
 
+  @Test
+  public void testWordsLettersLines()
+  {
+    PixelCompare pc = new PixelCompare(PATH);
+    pc.assertEqual(WordsLettersLines.class.getName());
+  }
 
   public static void main(String[] args)
   {
     PixelCompare pc = new PixelCompare(PATH);
-    pc.generateRefImage(WordsLettersLines.class.getName());
-    //pc.visualDiff(WordsLettersLines.class.getName());
+    pc.generateRefImage(DefaultFontAndFontVlwTtf.class.getName());
+    //pc.visualDiff(DefaultFontAndFontVlwTtf.class.getName());
   }
-
 
 }
