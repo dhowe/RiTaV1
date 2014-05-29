5dc6dc4349dc1a2d1ebe05075b4ec41fa8c0c430
Kenny Wong <kennyviperhk@gmail.com>
2014-05-29 17:37:11 +0800

    Timer and Color/Fade tests
    Former-commit-id: 759a2b83ad6cb968752ec9377b8eded70afca4aa

--- src/rita/render/test/ColorAndFade.java
+++ src/rita/render/test/ColorAndFade.java
@@ -13,49 +13,50 @@ public class ColorAndFade extends PApplet {
 		RiText.defaultFont("Times", 20);
 		
 
-		RiText rt = new RiText(this, "ColorTo gray in 2 sec", 100,  50);
+		RiText rt = new RiText(this, "1.ColorTo gray in 2 sec", 100,  50);
 		rt.colorTo(200, 2);
 		rt.showBounds(true);
 
 		float[] c = {0,0,255};
-		RiText rt6 = new RiText(this, "ColorTo Bluein 2 sec" , 100, 100);
+		RiText rt6 = new RiText(this, "2.ColorTo Bluein 2 sec" , 100, 100);
 		rt6.colorTo(c,2);
 		rt6.showBounds(true);
-		new RiText(this, "ColorTo Blue 5s delay 5" , 100, 150).colorTo(c,5,2);
+		new RiText(this, "3.ColorTo Blue 5s delay 5" , 100, 150).colorTo(c,5,2);
 
-		RiText rt2 = new RiText(this, "fade out and in" , 100, 200);
+		RiText rt2 = new RiText(this, "4.fade out and in" , 100, 200);
 		rt2.fadeOut(2);
 
-		RiText rt3 = new RiText(this, "fade out delay and in" , 100, 250);
+		rt2.fadeIn(2,4);
+		
+		RiText rt3 = new RiText(this, "5.fade out delay 2s and in at 8s" , 100, 250);
 		rt3.fadeOut(2,2);
 
-		rt2.fadeIn(2,4);
 		rt3.fadeIn(2,8);
 		
-		RiText rt4 = new RiText(this, "fade out and in" , 100, 300);
+		RiText rt4 = new RiText(this, "6.fade out and in at 4s" , 100, 300);
 		rt4.showBounds(true); 
 		rt4.fadeOut(2);
+		rt4.fadeIn(2,4);
 
-		RiText rt5 = new RiText(this, "fade out delay and in" , 100, 350);
+		RiText rt5 = new RiText(this, "7.fade out delay 2s and in at 8s" , 100, 350);
 		rt5.showBounds(true);
 		rt5.fadeOut(2,2);
 		
-		rt4.fadeIn(2,4);
 		rt5.fadeIn(2,8);
 
 
-		RiText rt7 = new RiText(this, "Null" , 100, 400);
+		RiText rt7 = new RiText(this, "8.Null" , 100, 400);
 		rt5.showBounds(true);
-		rt7.textTo("Changed Text",3);
+		rt7.textTo("8.Changed Text",3);
 		
 		
-		RiText rt8 = new RiText(this, "Null" , 100, 450);
+		RiText rt8 = new RiText(this, "9.Null" , 100, 450);
 
-		rt8.textTo("Changed Text delay3s",3,5); 
+		rt8.textTo("9.Changed Text delay3s",3,5); 
 		
-		RiText rt9 = new RiText(this, "Null" , 100, 500);
+		RiText rt9 = new RiText(this, "10.Null" , 100, 500);
 		rt9.showBounds(true);
-		rt9.textTo("Changed Text delay3s",3,5);	
+		rt9.textTo("10.Changed Text delay3s",3,5);	
 		
 		background(255);
 
