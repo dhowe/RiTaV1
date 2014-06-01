7cf221885e9e86c36ff310296d4837ff04c8ec32
Daniel Howe <daniel@rednoise.org>
2014-06-01 18:50:17 +0800

    update defaultFill test
    Former-commit-id: 4bb3d61cfb49717f4766626f8f9e5ffbebe9d939

--- src/rita/render/test/BoundingBoxAndAlpha.java
+++ src/rita/render/test/BoundingBoxAndAlpha.java
@@ -38,26 +38,28 @@ public class BoundingBoxAndAlpha extends PApplet {
 			new RiText(this, "alpha " + (i * 10), 600, 
 					(i + 1) * 38).alpha(i * 10).draw();
 
-		RiText rt = new RiText("D.Fill_200", 50,  500);
-		rt.defaultFill(200);
-		RiText rt5 = new RiText("D.Fill_10", 50,  550);
-		rt5.defaultFill(10);
-		RiText rt2 = new RiText("D.Fill_200_50A", 220,  500);
-		rt2.defaultFill(200,50);
-		RiText rt6 = new RiText("D.Fill_200_0A", 220,  550);
-		rt6.defaultFill(200,0);
-		RiText rt3 = new RiText("D.Fill_RED", 450,  500);
-		rt3.defaultFill(255,0,0);
+		new RiText("D.Fill_200", 50,  500);
+		RiText.defaultFill(200);
+		new RiText("D.Fill_10", 50,  550);
+		RiText.defaultFill(10);
+		new RiText("D.Fill_200_50A", 220,  500);
+		RiText.defaultFill(200,50);
+		new RiText("D.Fill_200_0A", 220,  550);
+		RiText.defaultFill(200,0);
+		new RiText("D.Fill_RED", 450,  500);
+		RiText.defaultFill(255,0,0);
 		fill(0);
-		text(rt3.defaultFill()[0]+","+rt3.defaultFill()[1]+","+rt3.defaultFill()[2]+","+rt3.defaultFill()[3],450,  600);
-		RiText rt8 = new RiText("D.Fill_BLUE", 450,  500);
-		rt8.defaultFill(0,0,255);
-		RiText rt4 = new RiText("D.Fill_RED_100A", 650,  500);
-		rt4.defaultFill(255,0,0,100);
-		RiText rt7 = new RiText("D.Fill_RED_0A", 650,  550);
-		rt7.defaultFill(255,0,0,0);
+		float[] df = RiText.defaultFill();
+		text(df[0]+","+df[1]+","+df[2]+","+df[3],450,  600);
+		new RiText("D.Fill_BLUE", 450,  500);
+		RiText.defaultFill(0,0,255);
+		new RiText("D.Fill_RED_100A", 650,  500);
+		RiText.defaultFill(255,0,0,100);
+		new RiText("D.Fill_RED_0A", 650,  550);
+		RiText.defaultFill(255,0,0,0); // invisible
+		df = RiText.defaultFill();
 		fill(0);
-		text(rt7.defaultFill()[0]+","+rt7.defaultFill()[1]+","+rt7.defaultFill()[2]+","+rt7.defaultFill()[3],650,  600);
+		text(df[0]+","+df[1]+","+df[2]+","+df[3],650,  600);
 		
 		/*
 		RiText rt9 = new RiText("Random Color", 50,  650);
