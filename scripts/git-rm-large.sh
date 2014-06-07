7595e765abb123b826946acaf396c1f0ff4764eb
Daniel Howe <daniel@rednoise.org>
2014-06-08 01:10:51 +0800

    git cleanup
    Former-commit-id: 56d2cfb10810793dc3dc64b00b6893a148b2ca5b

--- scripts/git-rm-large.sh
+++ scripts/git-rm-large.sh
@@ -2,6 +2,6 @@ cd ..
 
 FPATH=/Users/dhowe/Documents/eclipse-workspace/RiTa/scripts/large_files.txt
 
-git filter-branch --tree-filter 'rm -rf `cat $FPATH | cut -d " " -f 2` ' --prune-empty master
+git filter-branch -f --tree-filter 'rm -rf `cat $FPATH | cut -d " " -f 2` ' --prune-empty master
 
 echo done
