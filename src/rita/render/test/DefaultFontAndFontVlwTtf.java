a37c0899ebf1854eb9511dee816253c7b27de94f
Daniel Howe <daniel@rednoise.org>
2014-06-07 14:17:19 +0800

    Fixes to font-handling in RiText
    Former-commit-id: 3f050008636a9a3cb01eac35599cc13c5e040929

--- src/rita/render/test/DefaultFontAndFontVlwTtf.java
+++ src/rita/render/test/DefaultFontAndFontVlwTtf.java
@@ -12,56 +12,53 @@ public class DefaultFontAndFontVlwTtf extends processing.core.PApplet
 	  size(800, 600);
 
 	  RiText.defaults.alignment = CENTER;
+	  //RiText.defaults.showBounds = true;
 
 	  ///////////////////// statics ////////////////////////
-
 	  RiText.defaultFont("Courier");
-	  rt = new RiText(this, "CourierDefault", 200, 50);
+	  rt = new RiText(this, "CourierDefault(14)", 200, 50);
 
 	  RiText.defaultFont("Courier", 18);  
 	  rt = new RiText(this, "CourierDefault-18", 200, 100);
 
 	  RiText.defaultFont("Ziggurat32.vlw");
-	  rt = new RiText(this, "Default-Zig-vlw", 200, 170);
-	  
-	  //System.out.println(RiText.defaults.fontFamily+"/"+RiText.defaults.fontSize+
-	  //"\n"+rt.font()+"/"+ ((PFont)rt.font()).getSize()+"/fontSize="+rt.fontSize());
+	  rt = new RiText(this, "Default-Zig-vlw(32)", 200, 160);
+	  //text(rt.fontSize(), 200, 200);
+
 	  RiText.defaultFont("wendy.ttf");
-	  rt = new RiText(this, "wendy-ttf", 200, 210);
+	  rt = new RiText(this, "wendy-ttf(14)", 100, 210);
+
 	  ///////////////////// instances ////////////////////////
 
-	  rt = new RiText(this, "Courier", 200, 250);
+	  rt = new RiText(this, "Courier(14)", 100, 250);
 	  rt.font("Courier");
 
-	  rt = new RiText(this, "Courier18", 200, 300);
+	  rt = new RiText(this, "Courier18", 100, 300);
 	  rt.font("Courier", 18);
 
-	  rt = new RiText(this, "Zig-Vlw", 200, 370);
+	  rt = new RiText(this, "Zig-Vlw(32)", 200, 370);
 	  rt.font("Ziggurat32.vlw");
-
-	  rt = new RiText(this, "Zig-Vlw", 200, 450);
+	  text(rt.fontSize(), 200, 390);
+	  
+	  rt = new RiText(this, "Zig-Vlw(50)", 200, 475);
     rt.font("Ziggurat32.vlw");
     rt.fontSize(50);
+	  text(rt.fontSize(), 200, 520);
 
-	  text(rt.fontSize(), 200, 400);
-	  text(rt.fontSize(), 200, 480);
-
-	  // TODO: THESE 2 SEEM TO BE FAILING? ====================== 
-	  rt = new RiText(this, "wendy-20", 200, 520);
+	  rt = new RiText(this, "wendy-20", 600, 500);
 	  rt.font("wendy.ttf");
 	  rt.fontSize(20); 
 	  
-	  rt = new RiText(this, "wendy", 200, 560);  // should be mono-12?
-	  // ========================================================
+	  rt = new RiText(this, "wendy(14)", 600, 540); 
 	  
 	  RiText.defaultFont("Batang", 100);
 
-	  rt = new RiText(this, "Batang-80", 540, 280);
+	  rt = new RiText(this, "Batang-100", 490, 280);
 
 	  rt = new RiText(this, "Batang-14", 530, 20);
 	  rt.font("Batang", 14);
 
-	  rt = new RiText(this, "Batang-24", 530, 80);
+	  rt = new RiText(this, "Batang-24", 430, 80);
 	  rt.scale(24 / 100f);
 
 	  rt = new RiText(this, "Arial-50", 700, 170);
@@ -72,13 +69,11 @@ public class DefaultFontAndFontVlwTtf extends processing.core.PApplet
 
 	  RiText.defaultFont("Georgia", 32);
 
-	  rt = new RiText(this, "Default", 700, 80);
+	  rt = new RiText(this, "Georgia(32)", 700, 80);
 
-	  RiText rt2;
-	  rt2 = new RiText(this, "disposed and should not appear", 700, 100);
-	  
+	  RiText rt2 = new RiText(this, "disposed and should not appear", 400, 200);
 	  RiText.dispose(rt2);
-
+	  
 	  RiText.drawAll();
   }
   
--- src/rita/test/KnownIssuesTest.java
+++ src/rita/test/KnownIssuesTest.java
@@ -16,16 +16,29 @@ import rita.support.*;
 public class KnownIssuesTest implements Constants
 {
   @Test
+  public void testTokenizing()
+  {
+    String[] testStrings = { "The boy screamed, \"Where is my apple?\"", };
+
+    for (int i = 0; i < testStrings.length; i++)
+    {
+      String[] tokens = RiTa.tokenize(testStrings[i]);
+      String output = RiTa.untokenize(tokens);
+      equal(testStrings[i], output);
+    }
+  }
+
+  @Test
   public void testWordNetAntonyms()
   {
     // returns empty, but 'night' returns 'day'
-    
+
     RiWordNet w = new RiWordNet("/WordNet-3.1");
-    String[] result = w.getAntonyms("day","n");
-    //System.out.println(RiTa.asList(result)); 
-    setEqual(result, new String[]{"night"});
+    String[] result = w.getAntonyms("day", "n");
+    // System.out.println(RiTa.asList(result));
+    setEqual(result, new String[] { "night" });
   }
-  
+
   @Test
   public void testRhyming()
   {
@@ -34,7 +47,7 @@ public class KnownIssuesTest implements Constants
     String[] answer = new String[] { "ravage", "disparage", "cabbage", "etc" };
     deepEqual(answer, result);
   }
-  
+
   @Test
   public void testPosTagging()
   {
@@ -42,22 +55,22 @@ public class KnownIssuesTest implements Constants
     System.out.println(Arrays.asList(result));
     String[] answer = new String[] { "jj" };
     deepEqual(result, answer);
-    
+
     result = RiTa.getPosTags("shitting", false);
     System.out.println(Arrays.asList(result));
     answer = new String[] { "jj" };
     deepEqual(result, answer);
-    
+
     result = RiTa.getPosTags("shitty", false);
-  //System.out.println(Arrays.asList(result));
+    // System.out.println(Arrays.asList(result));
     answer = new String[] { "jj" };
     deepEqual(result, answer);
-    
+
     result = RiTa.getPosTags("shitty", true);
     answer = new String[] { "a" };
     deepEqual(result, answer);
   }
-  
+
   @Test
   public void testAnalyzeNums() // Cardinal numbers
   {
@@ -67,49 +80,51 @@ public class KnownIssuesTest implements Constants
     equal(features.get(PHONEMES), "w-ah-n-t-uw-th-r-iy");
     equal(features.get(SYLLABLES), "w-ah-n/t-uw/th-r-iy");
     equal(features.get(STRESSES), "0/0/0");
-    
+
     features = new RiString("1 2 7").analyze().features();
     ok(features);
-    System.out.println("Analyze: "+features);
+    System.out.println("Analyze: " + features);
     equal(features.get(PHONEMES), "w-ah-n t-uw s-eh-v-ax-n");
     equal(features.get(SYLLABLES), "w-ah-n t-uw s-eh-v/ax-n");
     equal(features.get(STRESSES), "1 1 1/0");
 
-    /* in RITAJS
-     * equal(features.get(SYLLABLES), "w-ah-n t-uw s-eh/v-ax-n");
-     * equal(features.get(STRESSES), "0 0 1/0");*/    
+    /*
+     * in RITAJS equal(features.get(SYLLABLES), "w-ah-n t-uw s-eh/v-ax-n");
+     * equal(features.get(STRESSES), "0 0 1/0");
+     */
 
     features = new RiString("123").analyze().features();
     ok(features);
     equal(features.get(PHONEMES), "w-ah-n-t-uw-th-r-iy");
     equal(features.get(SYLLABLES), "w-ah-n/t-uw/th-r-iy");
     equal(features.get(STRESSES), "0/0/0");
-    
-/*  In RITAJS  
- *  equal(features.get(PHONEMES), "w-ah-n-t-uw-th-r-iy");
-    equal(features.get(SYLLABLES), "w-ah-n/t-uw/th-r-iy");
-    equal(features.get(STRESSES), "0/0/0");
-*/
+
+    /*
+     * In RITAJS equal(features.get(PHONEMES), "w-ah-n-t-uw-th-r-iy");
+     * equal(features.get(SYLLABLES), "w-ah-n/t-uw/th-r-iy");
+     * equal(features.get(STRESSES), "0/0/0");
+     */
   }
-  
+
   @Test
-  public void testRandomIterator() {
+  public void testRandomIterator()
+  {
 
     JSONLexicon lexicon = JSONLexicon.getInstance();
     long ts = System.currentTimeMillis();
-    lexicon.randomPosIterator("nns"); 
-    System.out.println("randomPosIterator in "+(System.currentTimeMillis()-ts)+"ms");
-    equal("TODO:","fix performance!");
+    lexicon.randomPosIterator("nns");
+    System.out.println("randomPosIterator in " + (System.currentTimeMillis() - ts) + "ms");
+    equal("TODO:", "fix performance!");
   }
-  
+
   @Test
-  public void testSyllabify()   // ******
+  public void testSyllabify() // ******
   {
     RiLexicon lex = new RiLexicon();
     String phones = RiString.syllabify(LetterToSound.getInstance().getPhones("dragging"));
     String phones2 = lex.lexImpl.getRawPhones("dragging");
-    System.out.println(phones+" ?= "+phones2);
+    System.out.println(phones + " ?= " + phones2);
     equal(phones, phones2);
   }
-  
+
 }
