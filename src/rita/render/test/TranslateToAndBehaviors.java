38235e610892607071b265524610a0dfdaf4a2ca
Kenny Wong <kennyviperhk@gmail.com>
2014-07-23 18:28:45 +0800

    updated according to issues
    Former-commit-id: a556acdb800dbdcc045dc1e5844836ce4969d1d2

--- src/rita/render/test/TranslateToAndBehaviors.java
+++ src/rita/render/test/TranslateToAndBehaviors.java
@@ -29,51 +29,63 @@ public class TranslateToAndBehaviors extends PApplet
 
     RiText rt1 = new RiText(this, "Move",750,750);
     rt1.moveTo(200,100,2);
+    rt1.showBounds(true);
 
     rt3 = new RiText(this, "Move Delay2s");
     rt3.position(200, 250);
+    rt3.showBounds(true);
    
     rt3.moveTo(350, 100, 2,2);
 
     rt4 = new RiText(this, "Rotate",100,300);
     rt4.rotate(50).fill(255,0,0);
+    rt4.showBounds(true);
 
     rt5 = new RiText(this, "RotateTo Delay2s",500,300);
     rt5.fill(255,0,0);
     rt5.rotateTo(-2,2,2);
+    rt5.showBounds(true);
     
     rt6 = new RiText(this, "RotateTo",300,300);
     rt6.fill(255,0,0);
     rt6.rotateTo(0.5f,1);
+    rt6.showBounds(true);
     
     rt7 = new RiText(this, "Bigger",50,500);
     rt7.fill(0,200,0);
     rt7.scale(1.5f);
+    rt7.showBounds(true);
     
     rt8 = new RiText(this, "Taller",300,500);
     rt8.fill(0,200,0);
     rt8.scale(1.5f,2f);
+    rt8.showBounds(true);
     
     rt9 = new RiText(this, "ScaleTo",50,700);
     rt9.fill(0,200,0);
     rt9.scaleTo(1.5f,2);
+    rt9.showBounds(true);
     
     RiText rt10 = new RiText(this, "ScaleTo Delay2s",300,700);
     rt10.fill(0,200,0);
     rt10.scaleTo(1.5f,2,2);
+    rt10.showBounds(true);
 
     rt11 = new RiText(this, "Stop Behavior 100");
     rt11.position(0, 20);
+    rt11.showBounds(true);
    
     behaviourID = rt11.moveTo(800, 20, 5);
     
     rt12 = new RiText(this, "Stop Behavior 300");
     rt12.position(0, 20);
+    rt12.showBounds(true);
    
     behaviourID2 = rt12.moveTo(800, 20, 5);
     
     rt13 = new RiText(this, "Stop Behaviors");
     rt13.position(0, 80);
+    rt13.showBounds(true);
    
     rt13.moveTo(800, 80, 5);
 
