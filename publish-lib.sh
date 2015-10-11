5f82f2b5977c32cf6d1d5d55427479b2d8e5a276
Daniel Howe <daniel@rednoise.org>
2015-10-11 13:49:42 +0800

    Update to v

--- publish-lib.sh
+++ publish-lib.sh
@@ -5,18 +5,37 @@
 
 set -e
 
-if [ $# != 1 ]
+if [ $# == 0 ]
 then
   echo
   echo "publish RiTa resources to npm and rednoise"
   echo
-  echo "usage: publish-lib.sh [tag]"
+  echo "usage: publish-lib.sh [tag] (-w)"
   exit
 fi
 
+WEB_ONLY=0
+
+while [ $# -ge 1 ]; do
+    #echo arg: $1
+    case $1 in
+        -w) WEB_ONLY=1  ;;
+    esac
+    shift
+done
+
 ant -f resources/build.xml build.js
 ant -f resources/build.xml build
-~/bin/git-tag.sh $1
-cd js && ~/bin/git-tag.sh $1 && cd ..
-ant -f resources/build.xml npm.publish
-ssh $RED "cd ~/git/RiTa && git pull" 
+if [ $WEB_ONLY = 1 ]
+then
+    echo "*** Updating web only (no tags or npm) ***" 
+    git commit -am "Update to v$VERSION"
+    git push
+else
+    echo "Updating tags and NPM to ${VERSION}"
+    exit;
+    ~/bin/git-tag.sh ${VERSION}
+    cd js && ~/bin/git-tag.sh ${VERSION} && cd ..
+    ant -f resources/build.xml npm.publish
+fi
+ssh $RED "cd ~/git/RiTa && git pull"  # update server
