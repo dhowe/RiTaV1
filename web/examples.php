964ef996e4f934e239b2f6ea8d4818508af86305
Daniel Howe <daniel@rednoise.org>
2015-10-12 00:54:58 +0800

    updates to examples page

--- web/examples.php
+++ web/examples.php
@@ -2,59 +2,68 @@
 <html>
 
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>Examples</title>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/style.css" type="text/css" />
-    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-    <script language="javascript" src="js/highlight.js"></script>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <title>Examples</title>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+  <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+  <script language="javascript" src="js/highlight.js"></script>
 </head>
 
 <body>
-    <?php include("header.php"); ?>
+  <?php include("header.php"); ?>
+    <div class="gd-section pad-top-large">
+      <div class="gd-center pad-large">
+        <div class="row">
+          <div class="col1"></div>
+          <div class="col10">
+            <h3 style="margin-left:-20px">Examples</h3>
 
+            <div class="examples">
+              <div class="exampleitem gd-4  mgd-6 sgd-12">
+                <p>HaikuGrammar</p>
+                <img src="img/sshaiku.png" alt="">
+                <p>
+                  <a href="examples/p5js/HaikuGrammar">p5js</a> <a href="examples/p5js/HaikuGrammar/code.js">(source)</a>
+                  <br>
+                  <a href="examples/dom/HaikuGrammar">dom </a> <a href="examples/dom/HaikuGrammar/code.js">(source)</a>
+                </p>
+              </div>
+              <div class="exampleitem gd-4  mgd-6 sgd-12">
+                <p>KafgensteinMarkov </p>
+                <img src="img/sskafgenstein.png" alt="">
+                <p> <a href="examples/p5js/Kafgenstein">p5js</a> <a href="examples/p5js/Kafgenstein/code.js">(source)</a>
+                  <br>
+                  <a href="examples/dom/Kafgenstein">dom </a> <a href="examples/dom/Kafgenstein/code.js">(source)</a></p>
+              </div>
+              <div class="exampleitem gd-4  mgd-6 sgd-12">
+                <p>KWICmodel </p>
+                <img src="img/sskwic.png" alt="">
+                <p> <a href="examples/p5js/KWICmodel">p5js</a> <a href="examples/p5js/KWICmodel/code.js">(source)</a>
+                  <br>
+                  <a href="examples/dom/KWICmodel">dom </a> <a href="examples/dom/KWICmodel/code.js">(source)</a></p>
+              </div>
+              <br>&nbsp;
+              <br>&nbsp;
+              <br>&nbsp;
+              <br>&nbsp;
+              <br>&nbsp;
+              <br>
+            </div>
+          </div>
+          <div class="col1"></div>
 
-<div class="gd-section pad-top-large">
-<div class="gd-center pad-large">
-<div class="row">
-    <div class="col1"></div>
-    <div class="col10">
-     <h3 style="margin-left:-20px">Examples</h3>
-     <div class="examples">
-          <div class="exampleitem gd-4  mgd-6 sgd-12">
-     <p>HaikuGrammar</p>
-     <img src="img/sshaiku.png" alt="">
-     <p>
-        <a href="examples/p5js/HaikuGrammar">p5js</a> <a href="examples/p5js/HaikuGrammar/code.js">(source)</a><br>
-        <a href="examples/dom/HaikuGrammar">dom </a> <a href="examples/dom/HaikuGrammar/code.js">(source)</a> 
-     </p>
-</div>
-   <div class="exampleitem gd-4  mgd-6 sgd-12">
-            <p>KafgensteinMarkov </p>
-            <img src="img/sskafgenstein.png" alt="">
-          <p>  <a href="examples/p5js/Kafgenstein">p5js</a> <a href="examples/p5js/Kafgenstein/code.js">(source)</a><br>
-            <a href="examples/dom/Kafgenstein">dom </a> <a href="examples/dom/Kafgenstein/code.js">(source)</a></p>
- </div>
-   <div class="exampleitem gd-4  mgd-6 sgd-12">
-            <p>KWICmodel </p>
-             <img src="img/sskwic.png" alt="">
-           <p> <a href="examples/p5js/KWICmodel">p5js</a> <a href="examples/p5js/KWICmodel/code.js">(source)</a><br>
-            <a href="examples/dom/KWICmodel">dom </a> <a href="examples/dom/KWICmodel/code.js">(source)</a></p>
-</div>
-</div>
-</div>
-<div class="col1"></div>
+        </div>
 
- </div>
+      </div>
+    </div>
+    <?php include("footer.php"); ?>
 
-</div>
-</div>
-   <?php include("footer.php"); ?>
-
-<!--
+      <!--
 End Site Content
 -->
 
 </body>
+
 </html>
