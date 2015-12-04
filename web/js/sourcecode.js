66ba99fcec0fa210d4a77d2525f6e2e4e3c8fc95
cqx931 <cqx931@gmail.com>
2015-12-05 01:21:02 +0800

    fix the yaml syntax bug

--- web/js/sourcecode.js
+++ web/js/sourcecode.js
@@ -1,17 +1,22 @@
 //displaying source code with syntax highlighting 
-
+//prism
    function getcode(url,id){
-         $.ajax({
+     $.ajax({
         url: url,
         dataType: "text",
         success: function (data)
         {
-          $(id).html(data);
+          var encoded = data.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+                 return '&#'+i.charCodeAt(0)+';';
+          });
+          $(id).html(encoded); 
           Prism.highlightElement($(id)[0]);
         }
       });
+
   }
 
 $(function() {
+  getcode("../../data/haiku.yaml","#grammar");
   getcode("code.js","#source-code");
 });  
