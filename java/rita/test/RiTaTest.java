a11617dcd27d2b8e70ce2457ecfa61727d98c261
Cyrus S <yuyhero@gmail.com>
2015-12-20 23:50:16 +0800

    Fix testGetSyllablesStringArray()

--- java/rita/test/RiTaTest.java
+++ java/rita/test/RiTaTest.java
@@ -985,30 +985,27 @@ public class RiTaTest
     String[] txt = { "The dog ran faster than the other dog.",
         "But the other dog was prettier." };
     String result = RiTa.getSyllables(txt);
-    String answer = "dh-ax d-ao-g r-ae-n f-ae-s/t-er dh-ae-n dh-ax ah-dh/er d-ao-g . b-ah-t dh-ax ah-dh/er d-ao-g w-aa-z p-r-ih-t/iy/er .";
+    String answer = "dh-ah d-ao-g r-ae-n f-ae/s-t-er dh-ae-n dh-ah ah/dh-er d-ao-g . b-ah-t dh-ah ah/dh-er d-ao-g w-aa-z p-r-ih/t-iy/er .";
     equal(result, answer);
 
     txt = new String[] { "The", "emperor", "had", "no", "clothes", "on." };
     result = RiTa.getSyllables(txt);
-    answer = "dh-ax eh-m-p/er/er hh-ae-d n-ow k-l-ow-dh-z aa-n .";
+    answer = "dh-ah eh-m/p-er/er hh-ae-d n-ow k-l-ow-dh-z aa-n .";
     equal(result, answer);
 
     txt = new String[] { "The", "Laggin", "Dragon" };
     result = RiTa.getSyllables(txt);
-    //System.out.println(result);
-    answer = "dh-ax l-ae/g-ih-n d-r-ae-g/aa-n";
+    answer = "dh-ah l-ae/g-ih-n d-r-ae/g-ah-n";
     equal(result, answer);
     
     txt = new String[] { "the", "laggin", "dragon" };
     result = RiTa.getSyllables(txt);
-    //System.out.println(result);
-    answer = "dh-ax l-ae/g-ih-n d-r-ae-g/aa-n";
+    answer = "dh-ah l-ae/g-ih-n d-r-ae/g-ah-n";
     equal(result, answer);
 
     txt = new String[] { "@#", "$%", "&*", "()" };
     result = RiTa.getSyllables(txt);
     answer = "@ # $ % & * ( )";
-    //System.out.println(result);
     equal(result, answer);
 
     txt = new String[] { "" };
