6129a54ed19c955e62d306c9bd572fa8659bc5cb
dhowe <daniel@rednoise.org>
2016-03-30 07:39:15 -0400

    script to update broken symlinks

---
+++ create-symlinks.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+set -e
+cd web
+ln -fs download.php download.html
+ln -fs examples.php examples.html
+ln -fs gallery.php gallery.html
+ln -fs index.php index.html
+ln -fs quickstart.php quickstart.html
+if [ -z "$1" ] 
+  then
+    echo
+else
+    ln -fs RiTa-$1.zip rita.zip
+    ls -l rita.zip
+fi
