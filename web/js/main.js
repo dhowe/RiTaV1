7a637e1003854e0732c96e5b2da07ffe2f9a9adc
cqx931 <cqx931@gmail.com>
2016-04-17 01:20:46 +0800

    fix website bugs

--- web/js/main.js
+++ web/js/main.js
@@ -2,14 +2,12 @@ jQuery(document).ready(function ($) {
 
   $('.toggle-nav').click(function () {
     $(".menu").slideToggle(300);
-    console.log("toggle-works");
   });
 
   //hide Github when scroll down
   $(window).scroll(function () {
     if ($(this).scrollTop() > 350) { //use `this`, not `document`
       $('#gitFork').hide(400);
-      console.log("git-works");
     }
     if ($(this).scrollTop() < 350) { //use `this`, not `document`
       $('#gitFork').show(400);
