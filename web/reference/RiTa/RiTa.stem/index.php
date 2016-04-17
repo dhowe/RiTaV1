c7207b76e1dde4eb2e41f90f1198e1158000cd24
Daniel Howe <daniel@rednoise.org>
2016-04-17 12:16:21 +0800

    removed piwik code from docs

--- web/reference/RiTa/RiTa.stem/index.php
+++ web/reference/RiTa/RiTa.stem/index.php
@@ -153,20 +153,5 @@ String y = RiTa.stem('cakes', RiTa.PORTER);</pre>
 <?php include("../../../footer.php"); ?>
 
 
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-  (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://rednoise.org/al/piwik/";
-    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-    _paq.push(["setSiteId", "1"]);
-    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code -->
-
 	</body>
 </html>
