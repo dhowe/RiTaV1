270c579740bdc45562324a32467daa984eac0de9
dhowe <daniel@rednoise.org>
2016-10-02 14:48:02 -0400

    remove donate, adjust twitter

--- web/footer.php
+++ web/footer.php
@@ -9,10 +9,12 @@
   <p><span class="col">
   Daniel C. Howe
   </p></span><br>
-  <h5>Donate</h5>
+  <a href="http://twitter.com/RiTaSoftware" data-show-count="false" class="twitter-follow-button" >Follow @RiTaSoftware</a>
+  <script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
+  <!--h5>Donate</h5>
    <a class="coinbase-button" data-code="583bdb779d2a040a8622b91c9f414d1a"
    data-button-style="custom_small" href="https://www.coinbase.com/checkouts/583bdb779d2a040a8622b91c9f414d1a">Donate</a>
-   <script src="https://www.coinbase.com/assets/button.js"></script>
+   <script src="https://www.coinbase.com/assets/button.js"></script-->
 </div>
 </div>
 
@@ -24,9 +26,8 @@
   <a href="https://www.npmjs.com/package/rita"><img src="https://img.shields.io/npm/v/rita.svg" alt="npm version"></a><br>
   <a href="<?php echo $path.'/cite.php'; ?>">How to cite RiTa</a><br>
   <a href="https://github.com/dhowe/RiTa"><span class="source">RiTa on Github</span></a> <br>
-  <a href="https://github.com/dhowe/RiTaJS"><span class="source">RiTa.js on Github</span></a><br>
-  <a href="http://twitter.com/RiTaSoftware" data-show-count="false" class="twitter-follow-button" >Follow @RiTaSoftware</a>
-  <script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
+  <a href="https://github.com/dhowe/RiTaJS"><span class="source">RiTa.js on Github</span></a>
+
 </p>
      </div>
    </div>
