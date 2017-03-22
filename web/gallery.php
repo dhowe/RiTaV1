99ef003cf770012e3e35d981150d6a2de600baae
cqx931 <cqx931@gmail.com>
2017-03-22 12:58:57 +0800

    add new piece to gallery

--- web/gallery.php
+++ web/gallery.php
@@ -35,6 +35,13 @@ Add your site or application content here
   <div class="gallery_home">
       <div class="row">
 
+       <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+        <a href="./gallery/HW00/hw00.html" target="new"><img class="gray" src="img/hm.jpg" ></a><br>
+        <a href="./gallery/HW00/hw00.html" target="new">HW-PM-R-00</a>
+        <p><span style="font-size:10px;">by David Hale</span></p>
+        <p class="gallery_home_spacer">&nbsp;</p>
+      </span>
+
       <span class="gallery_home_item gd-4  mgd-6 sgd-12">
         <a href="./gallery/BookshelfWriter" target="new"><img class="gray" src="img/bookshelfWriter.jpg" ></a><br>
         <a href="./gallery/BookshelfWriter" target="new">Bookshelf Writer</a>
