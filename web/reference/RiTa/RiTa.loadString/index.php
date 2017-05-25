7b86564e06dbe99915e5db59d17692abc1d8166a
dhowe <daniel@rednoise.org>
2017-05-25 20:46:15 +0800

    dict update and docs edits

--- web/reference/RiTa/RiTa.loadString/index.php
+++ web/reference/RiTa/RiTa.loadString/index.php
@@ -73,7 +73,7 @@
 							<th scope="row">Syntax</th>
 
 							<!-- SYNTAX  -->
-							<td><pre>RiTa.loadString(url);<br/>RiTa.loadString(url, callback);<br/>RiTa.loadString(url, pApplet);</pre></td>
+							<td><pre>RiTa.loadString(url);<br/><br/>RiTa.loadString(url, callback);<br/>RiTa.loadString(url, pApplet);<br/><br/>RiTa.loadString(url, callback, str);<br/>RiTa.loadString(url, pApplet, str);</pre></td>
 						</tr>
 
 
@@ -84,7 +84,7 @@
 							<!-- PARAMETERS  -->
 
 							<table cellpadding="0" cellspacing="0" border="0" class="sub-table">
-								<tr class=''><th width='25%' scope='row' class=nobold>String</th><td width='75%'>the filename from which to load the file</td></tr><tr class=''><th width='25%' scope='row' class=nobold>Function (optional: the callback, for JavaScript only)<br>or Object (optional: the PApplet, for Java only)<br></th><td width='75%'>If a function (in JavaScript), it will be called after the file is loaded (with the loaded string as its argument).<br><br>If no callback is supplied, or the argument is a PApplet(in Java), onRiTaEvent(re) will be called instead, with re.type() == RiTa.DATA_LOADED</td></tr><tr class=''><th width='25%' scope='row' class=nobold>String (optional)</th><td width='75%'>the characters with which to join the lines of the string (default = '\n') -- in some cases, a space character may be more useful...</td></tr>
+								<tr class=''><th width='25%' scope='row' class=nobold>String</th><td width='75%'>the filename from which to load the file</td></tr><tr class=''><th width='25%' scope='row' class=nobold>Function or Object (optional)</th><td width='75%'>A callback (in JS) or a PApplet (in Java)<br>If a callback function, it will be called after the file is loaded, with the loaded string as its argument</td></tr><tr class=''><th width='25%' scope='row' class=nobold>String (optional)</th><td width='75%'>with which to join the lines of the string (default = '\n') -- in some cases, a space character may be more useful</td></tr>
 							</table></td>
 						</tr>
 
@@ -120,7 +120,7 @@
 
 								<!-- EXAMPLE  -->
 								<!--img src="../../../img/RiTa-logo4.png" alt="example pic" /-->
-								<pre class="margin">txt = RiTa.loadString("kafka.txt");</pre>
+								<pre class="margin">txt = RiTa.loadString("kafka.txt"); <br>txt = RiTa.loadString('kafka.txt', callbackFun); // JS <br>txt = RiTa.loadString("kafka.txt", this, " "); &nbsp; // Java</pre>
 							</div></td>
 						</tr>
 
