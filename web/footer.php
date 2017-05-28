d2bf287385948a8105f03cb763dc172189b83547
dhowe <daniel@rednoise.org>
2017-05-28 19:19:45 +0800

    updated nontracking sharing buttons

--- web/footer.php
+++ web/footer.php
@@ -6,11 +6,47 @@
 
 <div class="gd-4 sgd-9 sfl">
   <div class="footercol">
-  <h5>Author</h5>
+  <h5 style="margin-bottom: .4em">Author</h5>
   <p><span class="col">
   Daniel C. Howe
   </p></span><br>
-  <a href="https://twitter.com/RiTaSoftware" class="twitter-follow-button" ><i></i> @RiTaSoftware</a>
+  <h5 style="margin-bottom: 0em; margin-top: 0em"></h5>
+  <!--a href="https://twitter.com/RiTaSoftware" class="twitter-follow-button" ><i></i> @RiTaSoftware</a-->
+
+<div id="sharing">
+
+  <!-- Sharingbutton Twitter -->
+  <a class="private-sharing-button__link" href="https://twitter.com/intent/tweet/?text=Computational%20literature%20with%20RiTa%20@RiTaSoftware&amp;url=https%3A%2F%2Frednoise.org%2Frita" target="_blank" aria-label="">
+    <div class="private-sharing-button private-sharing-button--twitter private-sharing-button--small"><div aria-hidden="true" class="private-sharing-button__icon private-sharing-button__icon--solid">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.44 4.83c-.8.37-1.5.38-2.22.02.93-.56.98-.96 1.32-2.02-.88.52-1.86.9-2.9 1.1-.82-.88-2-1.43-3.3-1.43-2.5 0-4.55 2.04-4.55 4.54 0 .36.03.7.1 1.04-3.77-.2-7.12-2-9.36-4.75-.4.67-.6 1.45-.6 2.3 0 1.56.8 2.95 2 3.77-.74-.03-1.44-.23-2.05-.57v.06c0 2.2 1.56 4.03 3.64 4.44-.67.2-1.37.2-2.06.08.58 1.8 2.26 3.12 4.25 3.16C5.78 18.1 3.37 18.74 1 18.46c2 1.3 4.4 2.04 6.97 2.04 8.35 0 12.92-6.92 12.92-12.93 0-.2 0-.4-.02-.6.9-.63 1.96-1.22 2.56-2.14z"/></svg>
+      </div>
+    </div>
+  </a>
+
+  <!-- Sharingbutton E-Mail -->
+  <a class="private-sharing-button__link" href="mailto:rita-questions@rednoise.org?subject=Re: RiTa" target="_self" aria-label="">
+    <div class="private-sharing-button private-sharing-button--email private-sharing-button--small"><div aria-hidden="true" class="private-sharing-button__icon private-sharing-button__icon--solid">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22 4H2C.9 4 0 4.9 0 6v12c0 1.1.9 2 2 2h20c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM7.25 14.43l-3.5 2c-.08.05-.17.07-.25.07-.17 0-.34-.1-.43-.25-.14-.24-.06-.55.18-.68l3.5-2c.24-.14.55-.06.68.18.14.24.06.55-.18.68zm4.75.07c-.1 0-.2-.03-.27-.08l-8.5-5.5c-.23-.15-.3-.46-.15-.7.15-.22.46-.3.7-.14L12 13.4l8.23-5.32c.23-.15.54-.08.7.15.14.23.07.54-.16.7l-8.5 5.5c-.08.04-.17.07-.27.07zm8.93 1.75c-.1.16-.26.25-.43.25-.08 0-.17-.02-.25-.07l-3.5-2c-.24-.13-.32-.44-.18-.68s.44-.32.68-.18l3.5 2c.24.13.32.44.18.68z"/></svg>
+      </div>
+    </div>
+  </a>
+
+  <!-- Sharingbutton Hacker News -->
+  <a class="private-sharing-button__link" href="https://news.ycombinator.com/submitlink?u=https%3A%2F%2Frednoise.org%2Frita&amp;t=Computational%20literature%20with%20RiTa" target="_blank" aria-label="">
+    <div class="private-sharing-button private-sharing-button--hackernews private-sharing-button--small"><div aria-hidden="true" class="private-sharing-button__icon private-sharing-button__icon--solid">
+  			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 140 140"><path fill-rule="evenodd" d="M60.94 82.314L17 0h20.08l25.85 52.093c.397.927.86 1.888 1.39 2.883.53.994.995 2.02 1.393 3.08.265.4.463.764.596 1.095.13.334.262.63.395.898.662 1.325 1.26 2.618 1.79 3.877.53 1.26.993 2.42 1.39 3.48 1.06-2.254 2.22-4.673 3.48-7.258 1.26-2.585 2.552-5.27 3.877-8.052L103.49 0h18.69L77.84 83.308v53.087h-16.9v-54.08z"></path></svg>
+      </div>
+    </div>
+  </a>
+
+  <!-- Sharingbutton Facebook -->
+  <a class="private-sharing-button__link" href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Frednoise.org%2Frita" target="_blank" aria-label="">
+    <div class="private-sharing-button private-sharing-button--facebook private-sharing-button--small"><div aria-hidden="true" class="private-sharing-button__icon private-sharing-button__icon--solid">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.77 7.46H14.5v-1.9c0-.9.6-1.1 1-1.1h3V.5h-4.33C10.24.5 9.5 3.44 9.5 5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4z"/></svg>
+      </div>
+    </div>
+  </a>
+    </div>
   <!--h5>Donate</h5>
    <a class="coinbase-button" data-code="583bdb779d2a040a8622b91c9f414d1a"
    data-button-style="custom_small" href="https://www.coinbase.com/checkouts/583bdb779d2a040a8622b91c9f414d1a">Donate</a>
@@ -33,7 +69,7 @@
 
 <div class="gd-4 sgd-9 sfl">
    <div class="footercol acknoledgment">
-  <h5>Acknowledgments</h5>
+  <h5 style="margin-bottom: .4em">Acknowledgments</h5>
 <p><span class="col">
 Kenny Wong <br>
 John Cayley<br>
