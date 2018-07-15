aaae5cd94f9d138ae22207f342bfd18f8b823a49
dhowe <daniel@rednoise.org>
2018-07-15 19:39:03 +0800

    commented sol lewitt generator

--- web/gallery.php
+++ web/gallery.php
@@ -2,7 +2,10 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,153 +18,152 @@
   <script src="js/vendor/modernizr-2.6.2.min.js"></script>
   <script language="javascript" src="js/highlight.js"></script>
 </head>
+
 <body>
-<!--[if lt IE 7]>
+  <!--[if lt IE 7]>
 <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
 
-<!--
-Add your site or application content here
-  Download    Reference     Examples    Gallery    Contact
--->
-<?php include("header.php"); ?>
+  <?php include("header.php"); ?>
 
-<div class="gd-section pad-top-large">
-<div class="gd-center pad-large">
-<div class="row">
-  <div class="col1"></div>
-  <div class="col10">
-     <h3 style="margin-left:-20px">Gallery</h3>
-  <div class="gallery_home">
+  <div class="gd-section pad-top-large">
+    <div class="gd-center pad-large">
       <div class="row">
-         <span class="gallery_home_item gd-4  mgd-6 sgd-12">
-        <a href="./gallery/SortingBot" target="new"><img src="img/sortingBot.png" ></a><br>
-        <a href="./gallery/SortingBot" target="new">Sorting Bot</a>
-        <p><span>Darius Kazemi</span></p>
-        <p class="gallery_home_spacer">&nbsp;</p>
-      </span>
+        <div class="col1"></div>
+        <div class="col10">
+          <h3 style="margin-left:-20px">Gallery</h3>
+          <div class="gallery_home">
+            <div class="row">
 
-         <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+
+              <span class="gallery_home_item gd-4  mgd-6 sgd-12">
         <a href="./gallery/BracketMemeBot" target="new"><img src="img/bracket.png" ></a><br>
         <a href="./gallery/BracketMemeBot" target="new">Bracket Meme Bot</a>
         <p><span>Darius Kazemi</span></p>
-        <p class="gallery_home_spacer">&nbsp;</p>
-      </span>
+              <p class="gallery_home_spacer">&nbsp;</p>
+              </span>
 
-        <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4  mgd-6 sgd-12">
         <a href="./gallery/DNA" target="new"><img src="img/poetryDNA.png" ></a><br>
         <a href="./gallery/DNA" target="new">Poetry.dna</a>
         <p><span>Matthew Halpenny</span></p>
-        <p class="gallery_home_spacer">&nbsp;</p>
-      </span>
+              <p class="gallery_home_spacer">&nbsp;</p>
+              </span>
 
-        <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+              <!--span class="gallery_home_item gd-4  mgd-6 sgd-12">
         <a href="./gallery/SolLewitt" target="new"><img src="img/sl.png" ></a><br>
         <a href="./gallery/SolLewitt" target="new">Sol Lewitt Generator</a>
         <p><span>Richard Lapham</span></p>
-        <p class="gallery_home_spacer">&nbsp;</p>
-      </span>
+              <p class="gallery_home_spacer">&nbsp;</p>
+            </span-->
 
-       <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4  mgd-6 sgd-12">
         <a href="./gallery/HW00/hw00.html" target="new"><img src="img/hm.jpg" ></a><br>
         <a href="./gallery/HW00/hw00.html" target="new">HW-PM-R-00</a>
         <p><span>by David Hale</span></p>
-        <p class="gallery_home_spacer">&nbsp;</p>
-      </span>
+              <p class="gallery_home_spacer">&nbsp;</p>
+              </span>
 
-      <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4  mgd-6 sgd-12">
         <a href="./gallery/BookshelfWriter" target="new"><img src="img/bookshelfWriter.jpg" ></a><br>
         <a href="./gallery/BookshelfWriter" target="new">Bookshelf Writer</a>
         <p><span style="font-size:11px;">by A.Refsgaard K. Solvåg R. Cereser</span></p>
-        <p class="gallery_home_spacer">&nbsp;</p>
-      </span>
+              <p class="gallery_home_spacer">&nbsp;</p>
+              </span>
 
-      <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4  mgd-6 sgd-12">
         <a href="http://sarahmak.es/nightTweets" target="new"><img src="img/nightTweet.png" ></a><br>
         <a href="http://sarahmak.es/nightTweets" target="new">#nightTweets</a>
         <p>by Sarah K Hallacher</p>
-        <p class="gallery_home_spacer">&nbsp;</p>
-      </span>
+          <p class="gallery_home_spacer">&nbsp;</p>
+        </span>
 
-      <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4  mgd-6 sgd-12">
         <a href="https://elmcip.net/creative-work/mirroring-tears-visages" target="new"><img src="img/mirroringTears.png" ></a><br>
         <a href="https://elmcip.net/creative-work/mirroring-tears-visages" target="new">Mirroring Tears: Visages</a>
         <p>by John Cayley Penny Florence</p>
-        <p class="gallery_home_spacer">&nbsp;</p>
-      </span>
+          <p class="gallery_home_spacer">&nbsp;</p>
+        </span>
 
-    <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4  mgd-6 sgd-12">
         <a href="./gallery/TheWholeBrilliantEnterprise" target="new"><img src="img/enterprise_sm.png" ></a><br>
         <a href="./gallery/TheWholeBrilliantEnterprise" target="new">The Whole Brilliant Enterprise</a>
         <p>by Office for Creative Research</p>
         <p class="gallery_home_spacer">&nbsp;</p>
       </span>
 
-    <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
       <a href="./gallery/MemoireInvolontaireNo1" target="new"><img src="img/memory.gif" ></a><br>
       <a href="./gallery/MemoireInvolontaireNo1" target="new">Memoire Involontaire No. 1</a>
           <p>by A. Braxton Soderman</p>
       <p class="gallery_home_spacer">&nbsp;</p>
     </span>
 
-    <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
         <a href="./gallery/VisualGrammars" target="new"><img src="img/visgram.jpg" ></a>
         <a href="./gallery/VisualGrammars" target="new"><br>Visual Grammars</a>
         <p>by Angela Ferraiolo</p>
         <p class="gallery_home_spacer">&nbsp;</p>
       </span>
-<!-- row2-->
-    <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <!-- row2-->
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
       <a href="./gallery/GameDefinitions" target="new"><img src="img/gamedefs.png" ></a><br>
       <a href="./gallery/GameDefinitions" target="new">Game Definitions</a>
       <p>by MolleIndustria</p>
       <p class="gallery_home_spacer">&nbsp;</p>
     </span>
 
-    <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
       <a href="./gallery/CornellsYokeMachine" target="new"><img src="img/cornell.jpg" ></a><br>
       <a href="./gallery/CornellsYokeMachine" target="new">Cornell's Yoke Machine</a>
       <p>by Janalyn Guo</p>
       <p class="gallery_home_spacer">&nbsp;</p>
     </span>
 
-    <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
       <a href="./gallery/ShanShui" target="new"><img src="img/shanshui.jpg" ></a><br>
       <a href="./gallery/ShanShui" target="new">Shan Shui</a>
       <p>by Chen Qianxun</p>
       <p class="gallery_home_spacer">&nbsp;</p>
     </span>
-  <!-- row3-->
+              <!-- row3-->
 
-      <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
         <a href="./gallery/TheGodardFilmGenerator" target="new"><img src="img/godard.png" ></a><br>
         <a href="./gallery/TheGodardFilmGenerator" target="new">The Godard Film Generator</a>
         <p>by Mark Sample</p>
         <p class="gallery_spacer">&nbsp;</p>
       </span>
 
-      <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
         <a href="./gallery/AboutNothingPlaces" target="new"><img src="img/creeley270.jpg" ></a><br>
         <a href="./gallery/AboutNothingPlaces" target="new">about nothing, places...</a>
         <p>by Patricia Tomaszek</p>
         <p class="gallery_spacer">&nbsp;</p>
       </span>
 
-       <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
         <a href="./gallery/RiTaTwitterbots"target="new"><img src="img/twitterbots.gif" ></a><br>
         <a href="./gallery/RiTaTwitterbots"target="new">RiTa Twitterbots</a>
         <p>by Mark Sample</p>
         <p class="gallery_spacer">&nbsp;</p>
       </span>
-  <!-- row4-->
-      <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <!-- row4-->
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
         <a href="./gallery/SpinState"target="new"><img src="img/spin_state2.png" ></a><br>
         <a href="./gallery/SpinState"target="new">SpinState</a>
         <p>by Michael Coppola</p>
         <p class="gallery_spacer">&nbsp;</p>
       </span>
 
-      <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+              <a href="./gallery/SortingBot" target="new"><img src="img/sortingBot.png" ></a><br>
+              <a href="./gallery/SortingBot" target="new">Sorting Bot</a>
+              <p><span>Darius Kazemi</span></p>
+              <p class="gallery_home_spacer">&nbsp;</p>
+              </span>
+
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
         <a href="./gallery/automatype" target="new"><img src="img/automatype.jpg" ></a><br>
         <a href="./gallery/automatype" target="new">Automatype</a>
         <p>by Daniel C. Howe</p>
@@ -169,50 +171,50 @@ Add your site or application content here
       </span>
 
 
-      <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
         <a href="./gallery/HarryPotterBookGenerator" target="new"><img src="img/HarryPotterGenerator.png" ></a><br>
         <a href="./gallery/HarryPotterBookGenerator" target="new">Harry Potter Book Generator</a>
         <p>by Rob Clouth</p>
         <p class="gallery_spacer">&nbsp;</p>
       </span>
-      
-      <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+
+              <span class="gallery_home_item gd-4 mgd-6 sgd-12">
         <a href="./gallery/Zuckifier/" target="new"><img src="img/zuckbaby.jpg" ></a><br>
         <a href="./gallery/Zuckifier/" target="new">Zuckifier</a>
         <p>by Sam Lavigne</p>
         <p class="gallery_spacer">&nbsp;</p>
       </span>
 
-        <span class="gallery_home_item gd-4  mgd-6 sgd-12">
+              <span class="gallery_home_item gd-4  mgd-6 sgd-12">
         <a href="./gallery/InvokeObject" target="new"><img src="img/invokeObject.png" ></a><br>
         <a href="./gallery/InvokeObject" target="new">invoke(Object object);</a>
         <p>by Ben Nicholson</p>
         <p class="gallery_home_spacer">&nbsp;</p>
       </span>
-<!--       <span class="gallery_home_item gd-4 mgd-6 sgd-12">
+              <!--       <span class="gallery_home_item gd-4 mgd-6 sgd-12">
         <a href="https://play.google.com/store/apps/details?id=processing.test.jumping_wild" target="new"><img src="img/jumping.png" ></a><br>
         <a href="https://play.google.com/store/apps/details?id=processing.test.jumping_wild" target="new">Jumping Wild Butoh Training</a>
         <p>by Honza Svasek </p>
         <p class="gallery_spacer">&nbsp;</p>
       </span> -->
+            </div>
+
+          </div>
+        </div>
+        <div class="col1"></div>
+
+      </div>
     </div>
-
   </div>
-  </div>
-    <div class="col1"></div>
-
-</div>
-</div>
-    </div>
 
 
+  <?php include("footer.php"); ?>
 
-<?php include("footer.php"); ?>
 
-
-<!--
+  <!--
 End Site Content
 -->
 
 </body>
+
 </html>
