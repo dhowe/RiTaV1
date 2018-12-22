3c0342640b351281e6fdaebf97e01928b32a551c
cqx931 <cqx931@gmail.com>
2018-12-22 14:32:33 -0500

    add download banner

--- web/header.php
+++ web/header.php
@@ -27,8 +27,18 @@
     </ul>
 
     <a class="toggle-nav" href="#">&#9776;</a>
+    <div id="downloadBanner">
+    <a class="buttonlink" href="download.php"><span class="button">Download RiTa</span></a>
+      <div class="buttons">
+      <iframe src="http://ghbtns.com/github-btn.html?user=dhowe&amp;repo=RiTa&amp;type=watch&amp;count=true&amp" allowtransparency="true" frameborder="0" scrolling="0" width="100" height="21"></iframe>
+<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="I&#39;m coding language with RiTa" data-url="https://rednoise.org/rita/" data-hashtags="RiTa" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+    </div>
     <div class="gitwrap">
-     <a href="https://github.com/dhowe/RiTa/"><img id="gitFork" style="" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a></div>
+
+       <img id="gitFork" style="" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png">
+ <a class="triangleHref" href="https://github.com/dhowe/RiTa/"></a>
+     </div>
 
 <script language="javascript">
 
@@ -49,12 +59,19 @@
     }
   }
 
+  function showDownloadBanner(page) {
+    if (page == extractPageName(document.getElementById("nav").getElementsByTagName("a")[0].href))
+        document.getElementById("downloadBanner").className += "visible";
+  }
+
   (function() {
     hrefString = document.location.href ?
       document.location.href : document.location;
     if (document.getElementById("nav")!=null)
+      var page = extractPageName(hrefString)
       setActiveMenu(document.getElementById("nav").getElementsByTagName("a"),
-        extractPageName(hrefString));
+        page);
+      showDownloadBanner(page);
   })();
 
 </script>
