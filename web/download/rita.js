7b10e3facd04c05d7740a341f277050581d7894e
dhowe <daniel@rednoise.org>
2019-02-23 22:16:20 +0800

    Release v1.3.89

--- web/download/rita.js
+++ web/download/rita.js
@@ -158,7 +158,7 @@ var FEATURES = [ 'tokens', 'stresses', 'phonemes', 'syllables', 'pos', 'text' ];
 
 var RiTa = {
 
-  VERSION: '1.3.88',
+  VERSION: '1.3.89',
 
   /* For tokenization, Can't -> Can not, etc. */
   SPLIT_CONTRACTIONS: false,
