08295ef2cc03625c46f3ad4d622c4b5d79187767
cqx931 <cqx931@gmail.com>
2019-06-26 01:20:32 -0400

    fix to lab-lamp https problem for home page

--- web/index.php
+++ web/index.php
@@ -196,8 +196,8 @@ with or without Processing<br>(also in Android)</p>
       </span>
 
     <span class="gallery_home_item gd-4 mgd-6 sgd-12">
-      <a href="./gallery/MemoireInvolontaireNo1" target="new"><img src="img/memory.gif" width="180"></a><br>
-      <a href="./gallery/MemoireInvolontaireNo1" target="new">Memoire Involontaire No. 1</a>
+      <a href="http://rednoise.org/rita/gallery/MemoireInvolontaireNo1" target="new"><img src="img/memory.gif" width="180"></a><br>
+      <a href="http://rednoise.org/rita/gallery/MemoireInvolontaireNo1" target="new">Memoire Involontaire No. 1</a>
           <p>by A. Braxton Soderman</p>
       <p class="gallery_home_spacer">&nbsp;</p>
     </span>
