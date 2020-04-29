12c88b21fbbc4b2524b7b71c16518c6c68c0ac05
Kenny Wong <kennyviperhk@gmail.com>
2020-04-29 17:20:51 +0800

    moved to another branch

--- web2/gallery/TeachingModuleGenerator/index_files/semantic-linkbacks.js
+++
@@ -1,7 +0,0 @@
-jQuery( document ).ready( function() {
-  jQuery( '.additional-facepile' ).hide();
-
-  jQuery( '.toggle-additional-facepiles' ).click( function() {
-    jQuery( this ).parent( 'ul' ).find( '.additional-facepile' ).toggle();
-  } );
-} );
